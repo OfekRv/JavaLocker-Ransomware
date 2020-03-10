09a57d1dc808bef12a030a318488cccd32ef3ed8
OfekRv <ofekrv@gmail.com>
2020-03-10 12:56:17 +0200

    Update key.java

--- javabasic/key.java
+++ javabasic/key.java
@@ -1,599 +1,85 @@
-/*     */ package javabasic;
-/*     */ 
-/*     */ import java.io.FileInputStream;
-/*     */ import java.io.FileOutputStream;
-/*     */ import java.io.InputStream;
-/*     */ import java.io.OutputStream;
-/*     */ import java.security.Key;
-/*     */ import java.security.SecureRandom;
-/*     */ import javax.crypto.Cipher;
-/*     */ import javax.crypto.CipherInputStream;
-/*     */ import javax.crypto.CipherOutputStream;
-/*     */ import javax.crypto.KeyGenerator;
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ class key
-/*     */ {
-/*     */   Key key;
-/*     */   
-/*     */   public key(String str) {
-/* 527 */     getKey(str);
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public void getKey(String strKey) {
-/*     */     try {
-/* 534 */       KeyGenerator _generator = KeyGenerator.getInstance("DES");
-/* 535 */       _generator.init(new SecureRandom(strKey.getBytes()));
-/* 536 */       this.key = _generator.generateKey();
-/* 537 */       _generator = null;
-/* 538 */     } catch (Exception e) {
-/* 539 */       throw new RuntimeException("Error initializing SqlMap class. Cause: " + e);
-/*     */     } 
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public void encrypt(String file, String destFile) throws Exception {
-/* 550 */     Cipher cipher = Cipher.getInstance("DES");
-/*     */     
-/* 552 */     cipher.init(1, this.key);
-/* 553 */     InputStream is = new FileInputStream(file);
-/* 554 */     OutputStream out = new FileOutputStream(destFile);
-/* 555 */     CipherInputStream cis = new CipherInputStream(is, cipher);
-/* 556 */     byte[] buffer = new byte[1024];
-/*     */     int r;
-/* 558 */     while ((r = cis.read(buffer)) > 0) {
-/* 559 */       out.write(buffer, 0, r);
-/*     */     }
-/* 561 */     cis.close();
-/* 562 */     is.close();
-/* 563 */     out.close();
-/*     */   }
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */   
-/*     */   public void decrypt(String file, String dest) throws Exception {
-/* 573 */     Cipher cipher = Cipher.getInstance("DES");
-/* 574 */     cipher.init(2, this.key);
-/* 575 */     InputStream is = new FileInputStream(file);
-/* 576 */     OutputStream out = new FileOutputStream(dest);
-/* 577 */     CipherOutputStream cos = new CipherOutputStream(out, cipher);
-/* 578 */     byte[] buffer = new byte[1024];
-/*     */     int r;
-/* 580 */     while ((r = is.read(buffer)) >= 0) {
-/* 581 */       System.out.println();
-/* 582 */       cos.write(buffer, 0, r);
-/*     */     } 
-/* 584 */     cos.close();
-/* 585 */     out.close();
-/* 586 */     is.close();
-/*     */   }
-/*     */   
-/*     */   public static void main(String s, String r) throws Exception {
-/* 590 */     key td = new key("dsjfvif$$#%$#jvfdsjf@$@kjvfsfh@$#shrvehdf@$#$");
-/* 591 */     td.encrypt(s, r);
-/*     */   }
-/*     */ }
-
-
-/* Location:              C:\Users\Java\Desktop\malwares\JAVABASIC.jar!\javabasic\key.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+ package javabasic;
+ 
+ import java.io.FileInputStream;
+ import java.io.FileOutputStream;
+ import java.io.InputStream;
+ import java.io.OutputStream;
+ import java.security.Key;
+ import java.security.SecureRandom;
+ import javax.crypto.Cipher;
+ import javax.crypto.CipherInputStream;
+ import javax.crypto.CipherOutputStream;
+ import javax.crypto.KeyGenerator;
+ 
+ class key
+ {
+   Key key;
+   
+   public key(String str) {
+     getKey(str);
+   }
+ 
+ 
+   
+   public void getKey(String strKey) {
+     try {
+       KeyGenerator _generator = KeyGenerator.getInstance("DES");
+       _generator.init(new SecureRandom(strKey.getBytes()));
+       this.key = _generator.generateKey();
+       _generator = null;
+     } catch (Exception e) {
+       throw new RuntimeException("Error initializing SqlMap class. Cause: " + e);
+     } 
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+   
+   public void encrypt(String file, String destFile) throws Exception {
+     Cipher cipher = Cipher.getInstance("DES");
+     
+     cipher.init(1, this.key);
+     InputStream is = new FileInputStream(file);
+     OutputStream out = new FileOutputStream(destFile);
+     CipherInputStream cis = new CipherInputStream(is, cipher);
+     byte[] buffer = new byte[1024];
+     int r;
+     while ((r = cis.read(buffer)) > 0) {
+       out.write(buffer, 0, r);
+     }
+     cis.close();
+     is.close();
+     out.close();
+   }
+ 
+ 
+ 
+ 
+ 
+ 
+   
+   public void decrypt(String file, String dest) throws Exception {
+     Cipher cipher = Cipher.getInstance("DES");
+     cipher.init(2, this.key);
+     InputStream is = new FileInputStream(file);
+     OutputStream out = new FileOutputStream(dest);
+     CipherOutputStream cos = new CipherOutputStream(out, cipher);
+     byte[] buffer = new byte[1024];
+     int r;
+     while ((r = is.read(buffer)) >= 0) {
+       System.out.println();
+       cos.write(buffer, 0, r);
+     } 
+     cos.close();
+     out.close();
+     is.close();
+   }
+   
+   public static void main(String s, String r) throws Exception {
+     key td = new key("dsjfvif$$#%$#jvfdsjf@$@kjvfsfh@$#shrvehdf@$#$");
+     td.encrypt(s, r);
+   }
+ }
